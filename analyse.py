35a574b6ff28802bbfae55d34421b031e9b57d33
miyatsuki <miyatsuki.shiku@gmail.com>
2019-10-18 00:13:04 +0900

    first commit

---
+++ analyse.py
@@ -0,0 +1,118 @@
+from janome.tokenizer import Tokenizer
+import json
+import pathlib
+from tqdm import tqdm, trange
+import sys
+
+gram2_map = {}
+gram3_map = {}
+gram4_map = {}
+channel_word_map = {}
+
+path = pathlib.Path("video/")
+json_list = list(path.glob("*.json"))
+
+def getWords(tokenizer, text):
+    ans = []
+    for token in t.tokenize(text):
+        baseForm = token.base_form
+        pos = token.part_of_speech.split(",")
+        if (
+            pos[0] == "名詞"
+            and pos[1] == "一般"
+        ) or pos[0] == "カスタム名詞":
+            ans.append(baseForm)
+
+    return ans
+
+
+def getTokens(Tokenizer, text):
+    ans = []
+    for token in t.tokenize(text, wakati=True):
+        ans.append(token)
+    return ans
+
+
+def getNgrams(textArray, N):
+    ans = []
+    for i in range(len(textArray) - (N - 1)):
+        ans.append("".join(textArray[i : i + N]))
+    return ans
+
+
+def addMap(index, text, tokenizer):
+    if index not in channel_word_map:
+        channel_word_map[index] = {}
+
+    words = getWords(tokenizer, text)
+    for word in words:
+        if word not in channel_word_map[index]:
+            channel_word_map[index][word] = 0
+
+        channel_word_map[index][word] += 1
+
+    token_list = getTokens(tokenizer, text)
+    
+    gram2_list = getNgrams(token_list, 2)
+    for gram in gram2_list:
+        if gram not in gram2_map:
+            gram2_map[gram] = 0
+        gram2_map[gram] += 1
+
+    gram3_list = getNgrams(token_list, 3)
+    for gram in gram3_list:
+        if gram not in gram3_map:
+            gram3_map[gram] = 0
+        gram3_map[gram] += 1
+
+    gram4_list = getNgrams(token_list, 4)
+    for gram in gram4_list:
+        if gram not in gram4_map:
+            gram4_map[gram] = 0
+        gram4_map[gram] += 1
+
+
+t = Tokenizer("simpledic.csv", udic_type="simpledic", udic_enc="utf8")
+
+for i in trange(len(json_list)):
+    filePath = json_list[i]
+
+    with open(filePath, "r") as f:
+        info = json.load(f)
+
+        channelTitle = info["snippet"]["channelTitle"]
+        title = info["snippet"]["title"]
+        description = info["snippet"]["description"]
+
+        addMap(channelTitle, title, t)
+        addMap(channelTitle, description, t)
+
+
+num_lines = sum(1 for line in open('tweets.json'))
+with open('tweets.json', 'r') as f:
+    with tqdm(total = num_lines) as pbar:
+        for line in f:
+            data = json.loads(line.strip())
+
+            screen_name = data["screen_name"]
+            text = data["text"]
+
+            if screen_name not in channel_word_map:
+                channel_word_map[channelTitle] = {}
+
+            addMap(screen_name, text, t)
+            pbar.update(1)
+
+
+with open("channel_word_map.json", "w") as f:
+    json.dump(channel_word_map, f, ensure_ascii=False)
+
+
+with open("gram2_map.json", "w") as f:
+    json.dump(gram2_map, f, ensure_ascii=False)
+
+with open("gram3_map.json", "w") as f:
+    json.dump(gram3_map, f, ensure_ascii=False)
+
+with open("gram4_map.json", "w") as f:
+    json.dump(gram4_map, f, ensure_ascii=False)
